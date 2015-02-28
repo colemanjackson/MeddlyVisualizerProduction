1089651572ceecd43e9f8a6267d6c6006c510771
Coleman Jackson <jxnman93@gmail.com>
2015-02-28 17:17:43 -0600

    added system.in support, wip

--- MeddlyViewer/src/logging/Logger.java
+++ MeddlyViewer/src/logging/Logger.java
@@ -1,5 +1,20 @@
 package logging;
 
-public class Logger {
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 
+public class Logger {
+	PrintStream fileToWrite;
+
+	public Logger(PrintStream fileToWrite) {
+		this.fileToWrite = fileToWrite;
+	}
+
+	public Logger() {
+		this.fileToWrite = System.out;
+	}
+
+	public void log(String Message) {
+		fileToWrite.println(Message);
+	}
 }
